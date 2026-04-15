7c84093f71c16f3a4d1c8e9eea594f2ebb91d858
David Simansky <dsimansk@redhat.com>
2026-04-15 10:56:57 +0200

    Update generate.sh

--- openshift/generate.sh
+++ openshift/generate.sh
@@ -8,5 +8,4 @@ go run github.com/openshift-knative/hack/cmd/generate@latest \
   --root-dir "${repo_root_dir}" \
   --generators dockerfile \
   --includes cmd \
-  --app-file-fmt "/ko-app/%s" \
-  --dockerfile-image-builder-fmt "registry.ci.openshift.org/openshift/release:rhel-9-release-golang-1.25-openshift-4.21"
+  --app-file-fmt "/ko-app/%s"
